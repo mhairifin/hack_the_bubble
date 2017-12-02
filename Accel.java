ac6d37047f38a211b8a204a1b13930e1419119b7
guthrieb <joeguthrie2@hotmail.com>
2017-12-02 11:16:24 +0000

    Create Accel.java

---
+++ Accel.java
@@ -0,0 +1,19 @@
+public class Accel {
+    private double ax;
+    private double ay;
+
+    public Accel(double ax, double ay){
+        this.ax = ax;
+        this.ay = ay;
+    }
+
+    public double ax()
+    {
+        return this.ax;
+    }
+
+    public double ay()
+    {
+        return this.ay;
+    }
+}
